8585bb29b85ae3a73be1beb338839b50d6733ec6
jerry048 <61510650+jerry048@users.noreply.github.com>
2023-02-02 23:03:52 +0800

    Update seedbox_installation.sh

--- seedbox_installation.sh
+++ seedbox_installation.sh
@@ -5,7 +5,7 @@ function Update {
     normal_1; echo "Updating installed packages and install prerequisite"
     normal_2
     apt-get -qqy update && apt-get -qqy upgrade
-    apt-get -qqy install sudo sysstat htop curl psmisc
+    apt-get -qqy install sudo sysstat htop curl psmisc python3-pyqt5
     cd $HOME
     tput sgr0; clear
 }
